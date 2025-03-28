2c9f933e6b4da27fe0418fb6746b0796bcbdd352
Will <will@kindleman.com.au>
2025-03-28 14:26:41 +0000

    nested_items

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,20 +1,15 @@
-module Tractor
-  module Menus
-    module MenuHelper
-
-      def nested_items(items)
-        items.map do |item, sub_items|
-          content_tag(:div,
-                      (render(item) + content_tag(
-                        :div,
-                        nested_items(sub_items),
-                        class: "nested",
-                        'data-id': item.id
-                      )).html_safe,
-                      class: "list-group-item ",
-                      'data-id': item.id)
-        end.join.html_safe
-      end
-    end
+module MenuHelper
+  def nested_items(items)
+    items.map do |item, sub_items|
+      content_tag(:div,
+                  (render(item) + content_tag(
+                    :div,
+                    nested_items(sub_items),
+                    class: "nested",
+                    'data-id': item.id
+                  )).html_safe,
+                  class: "list-group-item ",
+                  'data-id': item.id)
+    end.join.html_safe
   end
 end
