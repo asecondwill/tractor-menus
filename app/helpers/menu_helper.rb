5006a6cb95501ca91e346f7b1cc5c224eb0e707c
Will <will@kindleman.com.au>
2025-03-28 14:25:59 +0000

    copy app stuff over

---
+++ app/helpers/menu_helper.rb
@@ -0,0 +1,20 @@
+module Tractor
+  module Menus
+    module MenuHelper
+
+      def nested_items(items)
+        items.map do |item, sub_items|
+          content_tag(:div,
+                      (render(item) + content_tag(
+                        :div,
+                        nested_items(sub_items),
+                        class: "nested",
+                        'data-id': item.id
+                      )).html_safe,
+                      class: "list-group-item ",
+                      'data-id': item.id)
+        end.join.html_safe
+      end
+    end
+  end
+end
